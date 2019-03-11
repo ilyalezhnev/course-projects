c067f682943667cded9fab218f187e06031a3df4
Ilya <kompfloss@gmail.com>
2019-03-11 23:23:26 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+var number = 5;
+var string = "Hello, World!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let persone = {
+  name: "John",
+  age: 25,
+  isMarried: false
+};
+
+console.log(persone.name);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[0]);
+
+// alert("Hello!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+ 
+// let answer = +prompt("Are you 18?", "Yes");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+console.log(isChecked || !isClose);
