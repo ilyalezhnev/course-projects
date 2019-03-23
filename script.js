12598d3db2118ca46008cf8b216b2d0d925be779
Ilya <kompfloss@gmail.com>
2019-03-23 19:55:22 +0300

    Add cycles

--- script.js
+++ script.js
@@ -132,9 +132,6 @@ for (let i = 1; i < 8; i++) {
   if (i == 6) {
     continue
   }
-
   console.log(i)
 }
 
-let a = [1, 2, 3]; let b = [1, 2, 3];
-console.log("1"[]);
--- workscript.js
+++ workscript.js
@@ -12,13 +12,61 @@ let appData = {
   savings: false
 };
 
-let i1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    h1 = prompt("Во сколько обойдется?", ""),
-    i2 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    h2 = prompt("Во сколько обойдется?", "");
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+      b = prompt("Во сколько обойдется?", "");
 
-appData.expenses.i1 = h1; // спросить у Дани как передается значение в свойство объекта
-appData.expenses.i2 = h2;
+  if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 20 ) {
+        console.log("Done!");
+        appData.expenses[a] = b;
+  } else {
+    alert("Неправильный формат ввода, попробуйте еще раз!");
+    console.log("Неправильный ввод");
+    i--; // Дописать: если пользователь ничего не ввел, то вернуться к вопросу заново
+  }
+};
 
-alert(appData.budget / 30);
+/* 
+let i = 0;
+while (i < 2) {
+  let a = prompt("sv", ""),
+      b = prompt("werg", "");
 
+  if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "", b !=0 && a.length < 20) {
+        appData.expenses[a] = b;
+  } else {
+    alert("Неправильный формат ввода, попробуйте еще раз!");
+    console.log("Неправильный ввод");
+    i--; // Дописать: если пользователь ничего не ввел, то вернуться к вопросу заново
+  }
+} */
+
+/* 
+let i = 0;
+do {
+  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+      b = prompt("Во сколько обойдется?", "");
+
+  if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 20 ) {
+        console.log("Done!");
+        appData.expenses[a] = b;
+  } else {
+    alert("Неправильный формат ввода, попробуйте еще раз!");
+    console.log("Неправильный ввод");
+    i--; // Дописать: если пользователь ничего не ввел, то вернуться к вопросу заново
+  }
+} */
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедевный бюджет составляет: " + appData.moneyPerDay + "руб.");
+
+if (appData.moneyPerDay < 500) {
+  console.log("Низкий уровень достатка");
+} else if (appData.moneyPerDay > 500 && appData.moneyPerDay < 2000) {
+  console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Высокий уровень достатка");
+} else {
+  console.log("Произошла ошибка!");
+}
