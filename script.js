272446eac68cecb2574542767d6d6ac73232c5aa
Ilya <kompfloss@gmail.com>
2019-03-25 22:53:32 +0300

    With arrays

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ console.log(isChecked || !isClose); */
 
 
 
-                    // Lesson 2
+                    // Lesson 2 Cycles
 
 
 
@@ -109,7 +109,7 @@ switch (num) {
 
 
 
-                    // Lesson 3 
+                    // Lesson 3 Cycles
 
 
 
@@ -135,3 +135,222 @@ for (let i = 1; i < 8; i++) {
   console.log(i)
 }
 
+
+
+
+
+                    // Lesson 4 Functions
+
+
+
+
+
+/* 
+function showFirstMessage(text) {
+  console.log(text);
+}
+
+showFirstMessage("Hello, World!");
+*/
+
+/*
+function calc(a, b) {
+  return (a + b);
+}
+console.log(calc(5, 255));
+*/
+
+// В ES6 появился новый синтаксис вызова функций
+
+let calc = (a, b) => a + b; // Объвление "стрелочной" функции
+
+console.log(calc(5, 27));
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2";
+console.log(Math.round(twelve));
+
+let twelve = "12.2px";
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
+
+let str = "man woman child kid";
+console.log( str.indexOf("man", 4) );
+
+
+
+let str = "Ослик Иа-Иа посмотрел на виадук"; // ищем в этой строке
+let target = "Иа"; // цель поиска
+
+let pos = 0;
+while (true) {
+  let foundPos = str.indexOf(target, pos);
+  if (foundPos == -1) break;
+
+  console.log( foundPos ); // нашли на этой позиции
+  pos = foundPos + 1; // продолжить поиск со следующей
+}
+
+
+
+let str = "ilya";
+let newStr = str[0].toUpperCase() + str.slice(1);
+console.log(newStr);
+
+
+
+
+
+                    // Lesson 5 Callback-function
+
+
+
+
+
+function first() {
+  setTimeout( function() {
+    console.log(1);
+    }, 500);
+}
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
+
+
+
+function learnJS(lang, callback) {
+  console.log("Я учу " + lang);
+  callback();
+}
+
+learnJS("Javascript", function() {
+  console.log("Я прошел 3-ий урок!");
+});
+
+
+
+
+
+                    // Lesson 5 Objects
+
+
+
+
+
+let options = {
+    width: 1024,
+    height: 1024,
+    name: "test"
+};
+
+console.log(options.name);
+options.bool = false; // добавляет свойство объекта
+options.colors = { // добавляемое свойство в свою очередь тоже является объектом
+  border: "black",
+  bg: "red"
+};
+
+delete options.bool; // удаляет свойство объекта
+
+console.log(options);
+
+for (let key in options) { // вывод свойств объекта со значениями
+  console.log("Свойство " + key + " имеет значение " + options[key]);
+}
+
+console.log(Object.keys(options).length);
+
+
+
+// Массивы
+
+
+
+let arr = [1, 2, 3, 4, 5];
+
+arr.pop(); // удаляет последний элемент массива
+arr.push("5"); // добавляет элемент из скобок последним
+arr.shift(); // удаляет первый элемент
+arr.unshift("1"); // добавляет элемент из скобок первым
+
+console.log(arr);
+
+for (i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+
+
+
+let arr = ["first", 2, 3, "four", 5];
+
+arr.forEach(function(item, i, mass) {
+  console.log(i + ": " + item + " (массив: " + mass + ")");
+});
+
+
+
+
+let mass = [1, 3, 4, 6, 7];
+
+for (let key in mass) { // for (... in ...) выводит ключи элементов массива
+  console.log(key);
+}
+
+let mass = [1, 3, 4, 6, 7];
+
+for (let key of mass) { // for (... of ...) выводит значения элементов массива
+  console.log(key);
+}
+
+
+
+let ans = prompt("", ""),
+    arr = [];
+
+arr = ans.split(",");
+console.log(arr);
+
+
+
+let arr = ["asfzv", "ttgs", "erg"],
+    i = arr.join(", ");
+
+console.log(i);
+
+
+
+let arr = [1, 15, 4],
+    i = arr.sort(compareNum);
+
+function compareNum(a,b) {
+  return a-b;
+}
+
+console.log(arr);
+
+
+
+
+let soldier = {
+  health: 400,
+  armor: 100
+};
+
+let john = {
+  health: 100
+};
+
+john.__proto__ = soldier;
+
+console.log(john);
+console.log(john.armor);
--- workscript.js
+++ workscript.js
@@ -1,7 +1,15 @@
-'use strict';
+let money, time;
 
-let money = +prompt("Ваш бюджет на месяц?", ""),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", "");
+  time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+  while(isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", "");
+  }
+}
+
+start();
 
 let appData = {
   budget: money,
@@ -9,64 +17,72 @@ let appData = {
   expenses: {},
   optionalExpenses: {},
   income: [],
-  savings: false
+  savings: true,
+  chooseExpenses: function() {
+    for (let i = 0; i < 2; i++) {
+      let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+          b = prompt("Во сколько обойдется?", "");
+    
+      if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 20 ) {
+            console.log("Done!");
+            appData.expenses[a] = b;
+      } else {
+          alert("Неправильный формат ввода, попробуйте еще раз!");
+          console.log("Неправильный ввод");
+          i--;
+      }
+    }
+  },
+  detectDayBudget: function() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Ежедевный бюджет составляет: " + appData.moneyPerDay + "руб.");
+  },
+  detectLevel: function() {
+    if (appData.moneyPerDay < 500) {
+      console.log("Низкий уровень достатка");
+    } else if (appData.moneyPerDay > 500 && appData.moneyPerDay < 2000) {
+      console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+      console.log("Высокий уровень достатка");
+    } else {
+      console.log("Произошла ошибка!");
+    }
+  },
+  checkSavings: function() {
+    if (appData.savings == true) {
+      let save = +prompt("Какова сумма накоплений?", ""),
+          percent = +prompt("Под какой процент?", "");
+  
+      appData.monthIncome = ( save / 100 / 12 * percent ).toFixed();
+      alert("Доход в месяц с Вашего депозита составит: " + appData.monthIncome + " руб.");
+    }
+  },
+  chooseOptExpenses: function() {
+    for(let i = 0; i < 3; i++) {
+      let opt = prompt("Введите статью необязательных расходов", "");
+      appData.optionalExpenses[i] = opt;
+    }
+  },
+  chooseIncome: function() {
+    for (i = 0; i < 1; i++) {
+      let items = prompt("Что принесет доп. доход? (Перечисли через запятую)", "");
+      if ( typeof(items) === 'string' && typeof(items) != null && items != '' ) {
+        appData.income = items.split(", ");
+        appData.income.push(prompt("Может что-то еще?", ""));
+        appData.income.sort();
+        console.log("Done!");
+      } else {
+          alert("Неправильный формат ввода, попробуйте еще раз!");
+          console.log("Неправильный ввод");
+          i--;
+      }
+    }
+    appData.income.forEach(function(item, i) {
+      alert("Способ доп.заработка: " + (i + 1) + " - " + item);
+    });
+  }
 };
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-      b = prompt("Во сколько обойдется?", "");
-
-  if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 20 ) {
-        console.log("Done!");
-        appData.expenses[a] = b;
-  } else {
-    alert("Неправильный формат ввода, попробуйте еще раз!");
-    console.log("Неправильный ввод");
-    i--; // Дописать: если пользователь ничего не ввел, то вернуться к вопросу заново
-  }
-};
-
-/* 
-let i = 0;
-while (i < 2) {
-  let a = prompt("sv", ""),
-      b = prompt("werg", "");
-
-  if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "", b !=0 && a.length < 20) {
-        appData.expenses[a] = b;
-  } else {
-    alert("Неправильный формат ввода, попробуйте еще раз!");
-    console.log("Неправильный ввод");
-    i--; // Дописать: если пользователь ничего не ввел, то вернуться к вопросу заново
-  }
-} */
-
-/* 
-let i = 0;
-do {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-      b = prompt("Во сколько обойдется?", "");
-
-  if ( typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 20 ) {
-        console.log("Done!");
-        appData.expenses[a] = b;
-  } else {
-    alert("Неправильный формат ввода, попробуйте еще раз!");
-    console.log("Неправильный ввод");
-    i--; // Дописать: если пользователь ничего не ввел, то вернуться к вопросу заново
-  }
-} */
-
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Ежедевный бюджет составляет: " + appData.moneyPerDay + "руб.");
-
-if (appData.moneyPerDay < 500) {
-  console.log("Низкий уровень достатка");
-} else if (appData.moneyPerDay > 500 && appData.moneyPerDay < 2000) {
-  console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-  console.log("Высокий уровень достатка");
-} else {
-  console.log("Произошла ошибка!");
+for (let key in appData) {
+  console.log("Наша программа включает в себя данные: " + key + " - " + appData[key]);
 }
